5a4b3584b96ee884176d57d39fdf37cdc4eadafa
Shodikin Amin <shodikin.amin.qwerty@gmail.com>
2024-07-13 18:41:20 +0800

    editPPDB-BelumAda

--- src/pages/admin/ppdb/EditPPDB.jsx
+++ src/pages/admin/ppdb/EditPPDB.jsx
@@ -1,21 +1,19 @@
 import React from "react";
 import HeaderLayout from "../../../layouts/admin/Header";
 import SiderLayout from "../../../layouts/admin/Sider";
-import DetailPPDBLayout from "../../../layouts/admin/DetailPPDB";
 import { TokenProvider } from "../../../components/data/AuthTokenContext";
 
-function DashboardPage() {
+function EditPPDB() {
   return (
     <TokenProvider>
       <div className="flex flex-row">
         <SiderLayout />
         <div className="h-screen w-full">
           <HeaderLayout />
-          <DetailPPDBLayout />
         </div>
       </div>
     </TokenProvider>
   );
 }
 
-export default DashboardPage;
+export default EditPPDB;
