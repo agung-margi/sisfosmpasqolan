69dd85c5a6ba822b0f36f89dc8e7a31e1e090e68
margiyonoagung <agung.margyono@gmail.com>
2024-07-14 01:40:13 +0700

    fix create ekskul

--- src/components/molecules/EkskulForm.jsx
+++ src/components/molecules/EkskulForm.jsx
@@ -1,25 +1,25 @@
-import React from 'react'
-import FieldText from '../atoms/Field';
+import React from "react";
+import FieldText from "../atoms/Field";
 
-const EkskulForm = () => {
+const EkskulForm = ({ initialValues, onChange }) => {
   const fields = [
-        { id: "extraName", label: "Nama Ekskul", placeholder: "Nama Ekskul" },
-        { id: "Catagory", label: "Katagori", placeholder: "Katagori" },
-        { id: "shortDesc", label: "Deskripsi Singkat", placeholder: "Deskripsi Singkat" },
-        { id: "fullDesc", label: "Deskripsi", placeholder: "Deskripsi" },
-        { id: "meetingDays", label: "Jadwal", placeholder: "Jadwal" },
-        { id: "coach", label: "Penanggung Jawab", placeholder: "Penanggung Jawab" },
-        { id: "location", label: "Lokasi Ekskul", placeholder: "Lokasi Ekskul" },
-        { id: "contactInfo", label: "Info Kontak", placeholder: "Info Kontak" },
+    { id: "extraName", label: "Nama Ekskul", placeholder: "Nama Ekskul" },
+    { id: "category", label: "Kategori", placeholder: "Kategori" },
+    { id: "shortDesc", label: "Deskripsi Singkat", placeholder: "Deskripsi Singkat" },
+    { id: "fullDesc", label: "Deskripsi", placeholder: "Deskripsi" },
+    { id: "meetingDays", label: "Jadwal", placeholder: "Jadwal" },
+    { id: "coach", label: "Penanggung Jawab", placeholder: "Penanggung Jawab" },
+    { id: "location", label: "Lokasi Ekskul", placeholder: "Lokasi Ekskul" },
+    { id: "contactInfo", label: "Info Kontak", placeholder: "Info Kontak" },
   ];
 
   return (
-  <>
-    {fields.map((field) => (
-      <FieldText key={field.id} {...field}/>
-    ))}
-  </>
-  )
-}
+    <>
+      {fields.map((field) => (
+        <FieldText key={field.id} id={field.id} label={field.label} placeholder={field.placeholder} value={initialValues[field.id] || ""} onChange={onChange} />
+      ))}
+    </>
+  );
+};
 
-export default EkskulForm
+export default EkskulForm;
